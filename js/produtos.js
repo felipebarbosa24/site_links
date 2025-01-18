c9d802d727c00765d900866ae9aafab96ada8184
jumentoquefezueli <jumentoquefezueli@gmail.com>
2025-01-18 13:00:00 -0300

    Primeiras estruturações com JS

--- js/produtos.js
+++ js/produtos.js
@@ -1,1 +1,3 @@
-window.alert("Escolha seu link de acordo com o nome do produto")
+var nome = window.prompt("Poderia nos informar seu nome?");
+document.write(`Olá, <strong>${nome}</strong>. Seja bem-vindo!<br>`);
+document.write(`<strong>${nome}</strong>, escolha seu link de acordo com o nome do produto:`);
